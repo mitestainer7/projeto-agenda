934b4ffb220b938fcb05599c8ea6e4e1573f7834
Nicolas Mitestainer <nicolas.mitestainer08@gmail.com>
2023-10-03 00:14:56 -0300

    cadastro usuário
    Foi criado o cadastro de usuário na plataforma

--- routes.js
+++ routes.js
@@ -4,9 +4,10 @@ const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
 
 // Rotas da home
-route.get('/index', homeController.index);
+route.get('/', homeController.index);
 
 // Rotas de login
 route.get('/login/index', loginController.index)
+route.post('/login/register', loginController.register)
 
 module.exports = route;
