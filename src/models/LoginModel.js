92608a8524eeeeaae1b20b16f11abc623c5db60a
Nicolas Mitestainer <nicolas.mitestainer08@gmail.com>
2023-10-03 22:30:33 -0300

    criado sistema de login de usuários
    nesse momento, foi criado a parte que realiza o login do usuário.

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const bcryptjs = require('bcryptjs')
+const bcryptjs = require('bcryptjs');
+const { async } = require('regenerator-runtime');
 
 const LoginSchema = new mongoose.Schema({
   email: { type: String, required: true },
@@ -16,6 +17,23 @@ class Login {
     this.user = null
   }
 
+  async login() {
+    this.valida()
+    if(this.errors.length > 0) return
+    this.user = await LoginModel.findOne({ email: this.body.email })
+
+    if(!this.user) {
+      this.errors.push('Usuário não cadastrado.')
+      return
+    }
+
+    if(!bcryptjs.compareSync(this.body.password, this.user.password)) {
+      this.errors.push('Senha inválida.')
+      this.user = null
+      return
+    }
+  }
+
   async register() {
     this.valida()
     if(this.errors.length > 0) return
@@ -27,18 +45,12 @@ class Login {
     const salt = bcryptjs.genSaltSync()
     this.body.password = bcryptjs.hashSync(this.body.password, salt);
 
-    try {
-      this.user = await LoginModel.create(this.body)
-    }
-    catch(e) {
-      console.log(e)
-    } 
+    this.user = await LoginModel.create(this.body)
   }
 
   async userExists() {
-    const user = await LoginModel.findOne({ email: this.body.email })
-
-    if(user) this.errors.push('Usuário já existente.')
+    this.user = await LoginModel.findOne({ email: this.body.email })
+    if(this.user) this.errors.push('Usuário já existente.')
   }
 
   valida() {
